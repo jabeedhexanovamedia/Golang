e0bdea5e88bb224139dad4cd31b0a16e61bceca0
Jabeed Syed <jabeedsyedinfo@gmail.com>
2025-12-07 15:22:12 +0530

    basic server, context

--- echo/main.go
+++ echo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,14 +18,46 @@ import (
 // 	e.Start(":8080")
 // }
 
+// func main() {
+// 	e := echo.New()
+
+// 	e.GET("/welcome", func(c echo.Context) error {
+// 		return c.JSON(http.StatusOK, map[string]string{
+// 			"message": "Lcome",
+// 		})
+// 	})
+
+// 	e.Logger.Fatal(e.Start(":1323"))
+// }
+
 func main() {
 	e := echo.New()
 
-	e.GET("/welcome", func(c echo.Context) error {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello")
+	})
+
+	e.GET("/json", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Lcome",
+			"data": "json data",
 		})
 	})
 
-	e.Logger.Fatal(e.Start(":1233"))
+	e.GET("/req", func(c echo.Context) error {
+
+		// res := map[string]interface{}{
+		// 	"method":  c.Request().Method,
+		// 	"host":    c.Request().Host,
+		// 	"url":     c.Request().URL.String(),
+		// 	"uri":     c.Request().RequestURI,
+		// 	"proto":   c.Request().Proto,
+		// 	"headers": c.Request().Header,
+		// }
+		// return c.JSON(200, res)
+		//
+		return c.String(200, fmt.Sprintf("%+v", c.Request()))
+	})
+
+	e.Logger.Fatal(e.Start("localhost:8080"))
+
 }
